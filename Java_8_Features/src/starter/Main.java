642f70797fb301711050d16d5114dcf6316424a0
krishna7kmr <kk77boe@gmail.com>
2025-04-26 16:23:52 +0530

    aa

--- Java_8_Features/src/starter/Main.java
+++ Java_8_Features/src/starter/Main.java
@@ -3,7 +3,10 @@ package starter;
 import entity.Employee;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +19,11 @@ public class Main {
         empList.add( new Employee(4,"Amit",22,900,"M","IT","Pune",2018) );
         empList.add( new Employee(5,"Rahim",40,1020,"M","HR","Kolkata",2019) );
 
+    // Q1. Grouping employee by city
 
+        Map<String, List<Employee> > empByCity= new HashMap<>();
+
+        empByCity= empList.stream().collect(Collectors.groupingBy(Employee::getCity));
+        System.out.println(empByCity);
     }
 }
